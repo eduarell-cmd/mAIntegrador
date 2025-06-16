46712a523d1261f96eed63d85d59e22a7030bf3e
XxLGTOxX <luisgerardoterrazasobregon@gmail.com>
2025-06-16 08:15:51 -0600

    .

--- backend/gemini.py
+++ backend/gemini.py
@@ -1,7 +1,11 @@
 from google import genai
 from google.genai import types
+import os
+from dotenv import load_dotenv
 
-client = genai.Client(api_key='AIzaSyBPAqwJz9qEFH0oAdVc-PsTly8TvaVhNMI') #API Key Gera
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents="""(Imagina que eres un espejo inteligente el cual te va a dar recomendaciones para tener un mejor dia dependiendo de como te vea y de tu estado de animo, no me digas como estoy ni como me siento, solo dime el consejo como si fuera una notificacion muy muy corta de un mensaje como si fueras un amigo, algo así como: "Por qué no vas a ver una pelicula con algun amigo o deberias de ..." obviamente dependiendo de la persona) Dame un consejo corto o que me recomiendas hacer para sentirme mejor (sexo: masculino, edad: 19 años, estado de animo: ({
     "es_misma_persona": true,
