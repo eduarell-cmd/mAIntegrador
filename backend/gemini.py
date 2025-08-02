2b76f74e3adce1a57176cdacdf2ad79cda50239b
Daniel Beltran <danielbeltran468jb@gmail.com>
2025-08-01 23:04:18 -0600

    singup actualizado para obtener datos de fecha de nacimiento y genero

--- backend/gemini.py
+++ backend/gemini.py
@@ -38,7 +38,7 @@ async def geminiprompt():
         "emocion_dominante": datos_emociones["emocion_dominante"]
     }
     emociones_json_str = json.dumps(emociones_para_prompt)
-
+    #TODO: LLenar con la infomacion del promt de forma dinamica segun el usuario
     prompt_text = f"""(Imagina que eres un espejo inteligente el cual te va a dar recomendaciones para tener un mejor dia dependiendo de como te vea y de tu estado de animo, no me digas como estoy ni como me siento, solo dime el consejo como si fuera una notificacion muy muy corta de un mensaje como si fueras un amigo, algo así como: "Por qué no vas a ver una pelicula con algun amigo o deberias de ..." obviamente dependiendo de la persona) Dame un consejo corto o que me recomiendas hacer para sentirme mejor (sexo: masculino, edad: 19 años, estado de animo: ({emociones_json_str}), preferencias: (gimnasio, futbol, peliculas, deportes), ubicación: (Chihuahua, Chihuahua, México), hora actual: 10:34 ) En ingles, solo muestra como si fuera la notificación, no me pongas ningun "aqui va" ni "aqui tienes" ni nada, solo el consejo, no me digas como estoy ni como me siento, solo dime el consejo como si fuera una notificacion muy muy corta de un mensaje como si fueras un amigo, algo así como: "Por qué no vas a ver una pelicula con algun amigo o deberias de ..."  pero en ingles, no me digas como estoy ni como me siento, solo dime el consejo como si fuera una notificacion muy muy corta de un mensaje como si fueras un amigo, algo así como: "Por qué no vas a ver una pelicula con algun amigo o deberias de ..." """
 
     try:
