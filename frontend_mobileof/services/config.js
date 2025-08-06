ad84dbb3d8aef0145ce58058b04da6ba1b4b88d7
eduarell-cmd <edu.arellanes9@gmail.com>
2025-08-05 19:30:06 -0600

    Mejora de ip pa q no la caguen

--- frontend_mobileof/services/config.js
+++ frontend_mobileof/services/config.js
@@ -1,3 +1,3 @@
 
-export const API_BASE_URL = "http://192.168.0.8:8000"; // Cambia TU_IP_LOCAL por la IP de tu PC 
+export const API_BASE_URL = "http://192.168.1.77:8000"; // Cambia TU_IP_LOCAL por la IP de tu PC 
 
